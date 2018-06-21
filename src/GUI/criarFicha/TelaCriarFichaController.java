e8cd423ec64a4a14843397ca9a4d9c08a874a8da
Renan Ramos <renanramos97@hotmail.com>
2018-06-21 09:42:53 -0300

    Update
    Reestruturação menor iniciada.

--- src/GUI/criarFicha/TelaCriarFichaController.java
+++ src/GUI/criarFicha/TelaCriarFichaController.java
@@ -1,4 +1,4 @@
-package GUI;
+package GUI.criarFicha;
 
 import java.net.URL;
 import java.util.ArrayList;
--- src/GUI/criarFicha/TelaRacasController.java
+++ src/GUI/criarFicha/TelaRacasController.java
@@ -1,10 +1,10 @@
-package GUI;
+package GUI.criarFicha;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-public class TelaClasseController implements Initializable {
+public class TelaRacasController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
