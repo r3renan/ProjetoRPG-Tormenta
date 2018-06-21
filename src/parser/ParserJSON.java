6a74de69742b9441c64ca72920e5644348e80884
Renan Ramos <renanramos97@hotmail.com>
2018-06-21 10:11:32 -0300

    GSON adicionado
    GSON adicionado

--- src/parser/ParserJSON.java
+++ src/parser/ParserJSON.java
@@ -1,5 +1,7 @@
 package parser;
 
+import com.google.gson.Gson;
+
 public class ParserJSON { //ATRIBUTOS: NOME, NIVEL, TEMPO DE EXECUÇÃO, ALCANCE, ALVOS, DURAÇÃO, TESTE DE RESISTENCIA, FONTE, DESCRIÇÃO
-    
+    Gson gson = new Gson();
 }
