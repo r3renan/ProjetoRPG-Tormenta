d2078b303203a70c52be140ca470bf33423c1b1f
Renan Ramos <renanramos97@hotmail.com>
2018-06-25 16:25:15 -0300

    Update

--- src/database/TABLE_MANOBRAS.java
+++ src/database/TABLE_MANOBRAS.java
@@ -1,5 +1,30 @@
 package database;
 
-public class TABLE_MANOBRAS {
+import static database.Database.executarUpdate;
+import parser.Manobra;
+
+public class TABLE_Manobras {
+    public static void gerarTable(){
+        String sql = "CREATE TABLE MANOBRAS("
+                + "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "PERICIA_REF TEXT NOT NULL,"
+                + "NOME TEXT NOT NULL,"
+                + "DESCRICAO TEXT NOT NULL,"
+                + "CLASSE_DIFICULDADE TEXT,"
+                + "FOREIGN KEY(PERICIA_REF) REFERENCES PERICIAS(NOME))";
+        
+        executarUpdate(sql);
+    }
     
+    public static void inserir(Manobra manobra){
+        String sql = "INSERT INTO MANOBRAS VALUES("
+                + "NULL,"
+                + "'" + manobra.getPericia_ref() + "',"
+                + "'" + manobra.getNome() + "',"
+                + "'" + manobra.getDescricao() + "',"
+                + "'" + manobra.getClasse_dificuldade() + "')";
+        
+        executarUpdate(sql);
+        System.out.println("Manobra " + manobra.getNome() + " inserida com sucesso.");
+    }
 }
